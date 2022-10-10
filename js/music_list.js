38ca8085f56cbf082861cc994413115f112bb158
Bobzycruiz <112961814+Bobzycruiz@users.noreply.github.com>
2022-10-10 15:01:53 +0100

    Add files via upload

---
+++ js/music_list.js
@@ -0,0 +1,20 @@
+let allMusic = [
+    {
+        name:"Numb",
+        artist:"Marshmello, Khalid",
+        img:"music-1",
+        src:"music-1"
+    },
+    {
+        name:"Fun - We Are Young",
+        artist:"Young ft. Janelle Monáe",
+        img:"music-2",
+        src:"music-2"
+    },
+    {
+        name:"All About That Bass",
+        artist:"Meghan Trainor",
+        img:"music-3",
+        src:"music-3"
+    },
+]
